eedd99449963c0052d0905f9c38fe038847f21f7
adridomm21 <131310408+adridomm21@users.noreply.github.com>
2023-04-23 16:25:35 -0400

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,23 +1,35 @@
-let num1 = 1;
-let num2 = 2;
-let num3 = 3; 
+const CALCULAR = document.getElementById('calcular');
+const ERROR = document.getElementById('error');
+const FLU = document.getElementById('flu');
+const MAN = document.getElementById('man');
 
-if (num1>num2 && num1>num3) {
-    console.log ("el mayor es num1 ");
-}else if (num2>num1 && num2>num3) {
-    console.log ("el mayor es " + num2 );
-}else {
-    console.log ("el mayor es " + num3);
-}
-
-function numeroMayor(num1, num2, num3){
-    if (num1>num2 && num1>num3) {
-        console.log ("el mayor es num1 ");
-    }else if (num2>num1 && num2>num3) {
-        console.log ("el mayor es " + num2 );
-    }else {
-        console.log ("el mayor es " + num3);
+CALCULAR.addEventListener('click', () => {
+    const DATO = document.getElementById('peso').value
+    if (DATO > 0){
+        ERROR.style.display = 'none'
+        let flujo = calcFlujo(DATO);
+        let mantenimiento = flujo*1500;
+        FLU.innerHTML = flujo + ' cc/hr';
+        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+    } else {
+        ERROR.style.display = 'block';
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
     }
-}
+})
 
-numeroMayor(num1, num2, num3)
+function calcFlujo(PESO) {
+    let resultado = 0;
+    if (PESO>0 && PESO<=10) { 
+        resultado = PESO * 100;
+    } else if (PESO<=20) {
+        resultado = (10*100)+((PESO-10)*50);
+    } else if (PESO<=30){
+        resultado =(10*100)+(10*50)+((PESO-20)*20);
+    } else if (PESO>30){
+        resultado = ((PESO*4)+7)/(PESO+90);
+    }   
+    return resultado; 
+}
